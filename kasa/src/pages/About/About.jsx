37ce7c3d9efc3e6d9080922e2cc30750f37a2afb
Vinylicious <matou2711@gmail.com>
2026-03-26 06:14:54 +0100

    Ajout About

--- kasa/src/pages/About/About.jsx
+++ kasa/src/pages/About/About.jsx
@@ -1,14 +1,54 @@
 
-
+import { useState} from 'react';
 import source_img from '../../images/source/Image_source_2.png';
 import './About.scss'
+import about_data from './About.json'
+
+
+
+function Aboutbutton({title,text}) {
+
+  const[inputstate, setinputstate] = useState(false);
+
+  const buttonclick = () => {
+    setinputstate(!inputstate);
+  };
+
+
+  return(
+    <div>
+      <div className='container_btn'>
+        <h2>{title}</h2>
+        <button className={inputstate ? 'button__on' : ''} onClick={buttonclick}><i className="fa-solid fa-chevron-up"></i></button>
+      </div>
+      <p className={inputstate ? 'show' : ''}>{text}</p>
+    </div>
+  );
+}
+
+
+function Listbutton(){
+  return(
+    <div className='container_list'>
+      {about_data.map((btn) =>(
+        <Aboutbutton 
+          key={btn.id}
+          title={btn.title}
+          text={btn.text}
+        />
+      ))}
+    </div>
+  )
+}
+
 
 
 function About() {
   return(
-      <div className='aboutbody'>
+      <main className='aboutbody'>
         <img src={source_img} alt="source" />
-      </div>
+        <Listbutton/>
+      </main>
     ) 
 }
 
